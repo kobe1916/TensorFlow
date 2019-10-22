8005a629b422028031cbd33af2cba1770120f654
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-10-22 22:47:19 +0800

    Update TensorFlow基本预算2.py

--- TensorFlow基本预算2.py
+++ TensorFlow基本预算2.py
@@ -34,3 +34,113 @@ init = tf.global_variables_initializer()
 sess.run(init)
 
 print(sess.run(result))
+
+
+#变量赋值
+
+#特殊情况需要人工更新的可用变量赋值语句
+#变量更新语句：
+#        update_op = tf.assign(variable_to_be_updates,new_value)
+
+#通过变量赋值输出1、2、 3 、、、10
+import tensorflow as tf
+
+value = tf.Variable(0,name='value')
+one = tf.constant(1)
+new_value = tf.add(value,one)
+update_value = tf.assign(value,new_value)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for _ in range(10):
+        sess.run(update_value)
+        print(sess.run(value))
+
+'''
+通过TensorFlow的变量赋值计算：1+2+3+...10？
+'''
+
+
+
+'''TensorFlow中的Variable变量类型  定义时需要初始化   但有些变量定义时  并不知道其数值  运行时 由外部输入
+如训练数据   此时需要用到占位符
+
+tf.placeholder 占位符
+'''
+
+
+#   tf.placeholder(dtype,shape = None,name = None)
+
+x = tf.placeholder(tf.float32,[2,3],name = 'tx')
+#此代码生成一个2*3的二维数组，矩阵中每个元素的类型都是tf.float32  
+
+
+
+
+#构建一个包含placeholder操作的计算图，在sessiomn中调用run方法时，
+#placeholder占用的变量比持续用feed_dict餐胡传递进去
+
+import tensorflow as tf
+
+a = tf.placeholder(tf.float32,name = 'a')
+b = tf.placeholder(tf.float32,name ='b')
+c = tf.multiply(a,b,name ='c')
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    #通过fees_dict的从参数传值，按字典格式
+    result = sess.run(c,feed_dict = {a:8.0,b:3.5})
+    
+    print(result)
+
+
+
+#多个操作可以通过一次Feed完成执行
+
+
+import tensorflow as tf
+
+a = tf.placeholder(tf.float32,name = 'a')
+b = tf.placeholder(tf.float32,name ='b')
+c = tf.multiply(a,b,name ='c')
+d = tf.subtract(a,b,name = 'd')
+
+init = tf.global_variables_initializer()
+
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    result = sess.run([c,d],feed_dict = {a:[8.0,2.0,3.5],b:[1.5,2.0,4.]})
+    
+    print(result)
+    #取结果的第一个
+    print(result[0])
+
+
+
+
+
+#一次返回多个值分别赋给多个变量
+
+import tensorflow as tf
+
+a = tf.placeholder(tf.float32,name = 'a')
+b = tf.placeholder(tf.float32,name ='b')
+c = tf.multiply(a,b,name ='c')
+d = tf.subtract(a,b,name = 'd')
+
+init = tf.global_variables_initializer()
+
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    rc,rd = sess.run([c,d],feed_dict = {a:[8.0,2.0,3.5],b:[1.5,2.0,4.]})
+    
+    
+    print("value of c = ",rc,"value of d = ",rd)
