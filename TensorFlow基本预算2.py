7c783024288d0bd6a14753756ec6a2da65b9edcb
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-10-22 22:46:01 +0800

    Update TensorFlow基本预算2.py

--- TensorFlow基本预算2.py
+++ TensorFlow基本预算2.py
@@ -1,1 +1,13 @@
+#常量constant   在运行过程中不会改变的单元，在TensorFlow中无需进行初始化操作
+
+import tensorflow as tf
+
+a = tf.constant(1.0,name = "a")
+b = tf.constant(2.5,name= "b")
+c = tf.add(a,b,name = "c")
+
+sess = tf.Session()
+c_value = sess.run(c)
+print(c_value)
+sess.close()
 
