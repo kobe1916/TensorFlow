3fd0e2819d821cd5f0ce53ed56367dd26089e7bd
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-10-22 22:46:20 +0800

    Update TensorFlow基本预算2.py

--- TensorFlow基本预算2.py
+++ TensorFlow基本预算2.py
@@ -11,3 +11,26 @@ c_value = sess.run(c)
 print(c_value)
 sess.close()
 
+
+
+#变量  Variable
+#在运行过程中会该改变的单元，在TensorFlow中须进行初始化操作
+
+'''
+个别变量初始化：
+            init_op = name_variable.initializer()
+所有变量初始化：
+            init_op = tf.global_variable_initializer()
+            '''
+node1 = tf.Variable(3.0,tf.float32,name = "node1")
+node2 = tf.Variable(4.0,tf.float32,name = "node2")
+result = tf.add(node1,node2,name = "add")
+
+sess = tf.Session()
+
+#变量初始化
+init = tf.global_variables_initializer()
+#init只是一个操作，也需要先运行才能实现（即所有操作只有运行后才能实现）
+sess.run(init)
+
+print(sess.run(result))
